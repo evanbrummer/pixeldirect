6a1d2e10fcf6a10402d91a345fe4386664b32241
Evan <77951826+evanbrummer@users.noreply.github.com>
2024-08-20 17:26:26 -0500

    Draw a line
    It's seems really slow, will investigate

--- main.cpp
+++ main.cpp
@@ -5,38 +5,46 @@
 #define HEIGHT 720
 
 int main(int argc, char *argv[]) {
-    std::cout << "Launching" << std::endl;
-
-    if (__cplusplus == 202101L) std::cout << "C++23";
-    else if (__cplusplus == 202002L) std::cout << "C++20";
-    else if (__cplusplus == 201703L) std::cout << "C++17";
-    else if (__cplusplus == 201402L) std::cout << "C++14";
-    else if (__cplusplus == 201103L) std::cout << "C++11";
-    else if (__cplusplus == 199711L) std::cout << "C++98";
-    else std::cout << "pre-standard C++." << __cplusplus;
-    std::cout << "\n";
-
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window *window = SDL_CreateWindow("Pixel Direct", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-
     if (window == NULL) {
         std::cout << "Error creating window: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
 
-    SDL_Event windowEvent;
+    SDL_Event e;
+    int x = 0, y = 0;
 
     while (true) {
-        if (SDL_PollEvent(&windowEvent)) {
-            if (windowEvent.type == SDL_QUIT) {
+        
+        void *pixels;
+        int pitch;
+        SDL_LockTexture(texture, NULL, &pixels, &pitch);
+
+        Uint32 *pixel_ptr = (Uint32*)((Uint8*)pixels + y * pitch + x * 4);
+        *pixel_ptr = SDL_MapRGBA(SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888), 255, 0, 0, 255);
+
+        if (x < WIDTH) x++;
+        if (y < HEIGHT) y++;
+
+        SDL_UnlockTexture(texture);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
                 break;
             }
         }
     }
 
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
