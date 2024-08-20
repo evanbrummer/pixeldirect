bcdac8b2b0ea1c6f0326048dbf2381cfb0181cca
Evan <77951826+evanbrummer@users.noreply.github.com>
2024-08-19 23:49:53 -0500

    Set up build shortcut

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-#include "tchar.h"
 #include "SDL.h"
 
 #define WIDTH 1280
 #define HEIGHT 720
 
 int main(int argc, char *argv[]) {
-    std::cout << "test" << std::endl;
+    std::cout << "Launching" << std::endl;
+
+    if (__cplusplus == 202101L) std::cout << "C++23";
+    else if (__cplusplus == 202002L) std::cout << "C++20";
+    else if (__cplusplus == 201703L) std::cout << "C++17";
+    else if (__cplusplus == 201402L) std::cout << "C++14";
+    else if (__cplusplus == 201103L) std::cout << "C++11";
+    else if (__cplusplus == 199711L) std::cout << "C++98";
+    else std::cout << "pre-standard C++." << __cplusplus;
+    std::cout << "\n";
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
